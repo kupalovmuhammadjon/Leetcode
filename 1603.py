c4736917b2ef4ca8724953283f229b379ff90e53
kupalovmuhammadjon <kupalovv.muhammadjon@gmail.com>
2024-01-01 12:53:01 +0500

    1603 solved

---
+++ 1603.py
@@ -0,0 +1,30 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.big = big
+        self.medium = medium
+        self.small = small
+        
+
+    def addCar(self, carType: int) -> bool:
+        if carType == 1:
+            if self.big:
+               self.big -= 1
+               return True
+            else:
+                return False
+        if carType == 2:
+            if self.medium:
+               self.medium -= 1
+               return True
+            else:
+                return False
+        else:
+            if self.small:
+               self.small -= 1
+               return True
+            else:
+                return False
+             
+        
+
