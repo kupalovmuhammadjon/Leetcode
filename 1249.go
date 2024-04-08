61130b6bfb639fe178ab263aae9b86426e32e3c7
Ko'palov Muhammadjon <kupalovv.muhammadjon@gmail.com>
2024-04-08 17:40:05 +0500

    Problems solved

---
+++ 1249.go
@@ -0,0 +1,26 @@
+func minRemoveToMakeValid(s string) string {
+    var result strings.Builder 
+    balance := 0
+    for _, c := range s {
+        if c == '(' {
+            balance++
+        } else if c == ')' {
+            if balance == 0 {
+                continue
+            }
+            balance--
+        }
+        result.WriteRune(c)
+    }
+    res := result.String()
+    i := len(res) - 1
+    for i >= 0 && balance > 0{
+        if res[i] == '('{
+            balance--
+            res = res[:i] + res[i + 1:]
+        }
+        i--
+    }
+
+    return res
+}
