5d679b0198201ee4601f4938bb7d91a8783ab1a2
Ko'palov Muhammadjon <kupalovv.muhammadjon@gmail.com>
2024-06-17 15:35:26 +0500

    Problems solved

---
+++ 185.sql
@@ -0,0 +1,24 @@
+select 
+    d.name as Department,
+    e.name as Employee,
+    e.salary as Salary
+from
+    employee as e
+join
+    department as d 
+on 
+    d.id = e.departmentId
+where
+    e.salary in (
+        select distinct
+            salary
+        from
+            employee as e2
+        where
+            e2.departmentId = e.departmentId
+        group by
+            salary
+        order by
+            salary desc
+        limit 3
+    );
